4fa3b4ca44b34928cd2b5b7bd8fda82f7ed89853
KH <killhamster@gmail.com>
2020-03-19 11:45:59 +0800

    further changes to include additional classes in title tags

--- pywikihow/__init__.py
+++ pywikihow/__init__.py
@@ -96,7 +96,7 @@ class HowTo:
 
     def _parse_title(self, soup):
         # get title
-        html = soup.findAll("h1", {"class": "title_lg"})[0]
+        html = soup.findAll("h1", {"class": ["title_lg", "title_md", "title_sm"]})[0]
         a = html.find("a")
         if not a:
             raise ParseError
